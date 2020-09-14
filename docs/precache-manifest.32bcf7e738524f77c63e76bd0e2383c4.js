99917113a28414ef59e0e891af1df8de00b67574
diegobrunetto <diego.brunetto2@gmail.com>
2020-09-14 18:57:30 -0300

    docs agregado

---
+++ docs/precache-manifest.32bcf7e738524f77c63e76bd0e2383c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521d35615fd99147dc7cdb15a80ac20e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91c0e28aebc3ca778d7",
+    "url": "/static/css/main.ac3bbeca.chunk.css"
+  },
+  {
+    "revision": "3f7819c0c29ae0b0e20e",
+    "url": "/static/js/2.e0d3fef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0d3fef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91c0e28aebc3ca778d7",
+    "url": "/static/js/main.1e365081.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
